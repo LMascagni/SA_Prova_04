ff2c13c294eef1596ac90b71c4bac16964a4572c
34346 <34346@ITTCV-SIS-09>
2024-04-20 13:35:31 +0200

    sium

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,88 @@
 #include <Arduino.h>
+#include <FastAccelStepper.h>
 
-// put function declarations here:
-int myFunction(int, int);
+FastAccelStepperEngine engine = FastAccelStepperEngine();
+FastAccelStepper *stepper = NULL;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define dirPinStepper 18
+#define enablePinStepper 16
+#define stepPinStepper 17
+
+#define M0 21
+#define M1 22
+#define M2 23
+
+int microstep;
+
+void IRAM_ATTR interruptStop();
+bool stopped = false;
+
+bool setStep(int steps_in)
+{
+   microstep = steps_in;
+   int steps = log10(steps_in) / log10(2);
+   digitalWrite(M0, steps & 0b00000001);
+   digitalWrite(M1, (steps >> 1) & 0b00000001);
+   digitalWrite(M2, (steps >> 2) & 0b00000001);
+   return true;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int angleToStep(int angle)
+{
+   return int(200 * microstep * (angle / 360.0));
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void IRAM_ATTR interruptStop()
+{
+   stepper->forceStop();
+   stopped = true;
+}
+
+void IRAM_ATTR interruptAck()
+{
+   stopped = false;
+}
+
+void setup()
+{
+   Serial.begin(115200);
+   pinMode(M0, OUTPUT);
+   pinMode(M1, OUTPUT);
+   pinMode(M2, OUTPUT);
+   engine.init();
+   stepper = engine.stepperConnectToPin(stepPinStepper);
+   pinMode(15, INPUT_PULLUP);
+   attachInterrupt(15, interruptStop, FALLING);
+   pinMode(4, INPUT_PULLUP);
+   attachInterrupt(4, interruptAck, FALLING);
+}
+
+void loop()
+{
+   if (stepper && !stopped)
+   {
+      stepper->setDirectionPin(dirPinStepper);
+      stepper->setEnablePin(enablePinStepper);
+      stepper->setAutoEnable(true);
+      stepper->setSpeedInHz(500);
+      stepper->setAcceleration(100);
+      setStep(4);
+      stepper->moveTo(angleToStep(360), true);
+
+      if (!stopped)
+      {
+         stepper->setSpeedInHz(500);
+         stepper->setAcceleration(200);
+         setStep(4);
+         stepper->moveTo(angleToStep(-360), true);
+      }
+
+      if (!stopped)
+      {
+         stepper->setSpeedInHz(1000);
+         stepper->setAcceleration(200);
+         setStep(4);
+         stepper->moveTo(angleToStep(0), true);
+      }
+   }
 }
