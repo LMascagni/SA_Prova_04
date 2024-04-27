f88f9d6dfe0031ad22d182e314f4e0b9f94d7b5a
LMascagni <126956415+LMascagni@users.noreply.github.com>
2024-04-27 12:29:13 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,20 @@
 #include <Arduino.h>
-#include <FastAccelStepper.h>
-
-FastAccelStepperEngine engine = FastAccelStepperEngine();
-FastAccelStepper *stepper = NULL;
-
-#define dirPinStepper 18
-#define enablePinStepper 16
-#define stepPinStepper 17
+#include "StepperMotor.h"
 
 #define M0 21
 #define M1 22
 #define M2 23
 
-int microstep;
-volatile bool stopped = false;
+StepperMotor myStepper(17, 18, 16); // Assuming step, dir, enable pin assignments
 
-bool setStep(int steps_in)
-{
-   microstep = steps_in;
-   int steps = log10(steps_in) / log10(2);
-   digitalWrite(M0, steps & 0b00000001);
-   digitalWrite(M1, (steps >> 1) & 0b00000001);
-   digitalWrite(M2, (steps >> 2) & 0b00000001);
-   return true;
-}
-
-int angleToStep(int angle)
-{
-   return int(200 * microstep * (angle / 360.0));
-}
-
-void moveStepper(int angle, float speed, float acceleration, int microsteps) {
-   stepper->setDirectionPin(dirPinStepper);
-   stepper->setEnablePin(enablePinStepper);
-   stepper->setAutoEnable(true);
-   stepper->setSpeedInHz(speed);
-   stepper->setAcceleration(acceleration);
-   setStep(microsteps);
-   stepper->moveTo(angleToStep(angle), true);
-}
-
-void IRAM_ATTR interruptStop()
-{
-   stepper->forceStop();
-   stopped = true;
-}
-
-void IRAM_ATTR interruptAck()
-{
-   stopped = false;
-}
-
-void setup()
-{
-   pinMode(M0, OUTPUT);
-   pinMode(M1, OUTPUT);
-   pinMode(M2, OUTPUT);
-
-   Serial.begin(115200);
-   
-   engine.init();
-   stepper = engine.stepperConnectToPin(stepPinStepper);
-   
-   pinMode(15, INPUT_PULLUP);
-   attachInterrupt(15, interruptStop, FALLING);
-   pinMode(4, INPUT_PULLUP);
-   attachInterrupt(4, interruptAck, FALLING);
+void setup() {
+    Serial.begin(115200);
+    myStepper.setMicrostep(4);  // Set microsteps in your sketch
 }
 
 void loop() {
-   moveStepper(1800, 500, 100, 4);
-   moveStepper(-1800, 500, 200, 4);
-   moveStepper(0, 1000, 200, 8);
+    myStepper.moveStepper(1800, 500, 100);
+    myStepper.moveStepper(-1800, 500, 200);
+    myStepper.moveStepper(0, 1000, 200);
+    // ... add more loop commands if needed
 }
